317dee3caa236960d21f89ad4a6ab76d729e862e
Thiago Veloso <thiago.vbs@gmail.com>
2019-02-27 00:02:47 -0300

    ícones e splash screen da aplicação

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG ={
-    baseUrl: "http://localhost:8080",
-    authUrl:"http://localhost:8080/oauth/token",
+    baseUrl: "https://sheap.herokuapp.com",
+    authUrl:"https://sheap.herokuapp.com/oauth/token",
     s3Url:"https://s3-sa-east-1.amazonaws.com/compre-certo"
 }
