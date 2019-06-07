4b25df83cd0976776a0aa0b9840857d987af6761
philipe amorim <philipe.lopes07@gmail.com>
2019-06-06 21:54:02 -0300

    url alterada

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,8 +1,8 @@
 export const API_CONFIG = {
     
-    baseUrl: "https://sheap.herokuapp.com/rest",
-    //baseUrl:"http://167.86.118.44:8080/rest",
-    authUrl: "https://sheap.herokuapp.com/oauth/token",
-    //authUrl: "http://167.86.118.44:8080/oauth/token",
+    //baseUrl: "https://sheap.herokuapp.com/rest",
+    baseUrl:"http://167.86.118.44:8080/rest",
+    //authUrl: "https://sheap.herokuapp.com/oauth/token",
+    authUrl: "http://167.86.118.44:8080/oauth/token",
     s3Url: "https://s3-sa-east-1.amazonaws.com/sheap-bucket"
 }
