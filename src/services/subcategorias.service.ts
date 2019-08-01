6c0d9853c729c4f23b83eac68f81cfe8403f95eb
philipe amorim <philipe.lopes07@gmail.com>
2019-08-01 19:20:22 -0300

    fix bug paginacao e pedidos

--- src/services/subcategorias.service.ts
+++ src/services/subcategorias.service.ts
@@ -28,8 +28,8 @@ export class SubCategoriaService {
         return this.http.get<any>(`${API_CONFIG.baseUrl}/mercado-produtos/dto/com-validade/?idCategoria=${categoriaId}&idMercado=${mercadoId}&page${page}&count=${count}`);
     }
 
-    findProdutosPorMercado(mercadoId): Observable<any> {
-        return this.http.get<any>(`${API_CONFIG.baseUrl}/mercado-produtos/dto/com-validade/?idMercado=${mercadoId}`);
+    findProdutosPorMercado(mercadoId, page:number = 0, count:number =10): Observable<any> {
+        return this.http.get<any>(`${API_CONFIG.baseUrl}/mercado-produtos/dto/com-validade/?idMercado=${mercadoId}&page=${page}&count=${count}`);
     }
 
     findProdutosComDtValidadeEbairro(idBairro): Observable<any> {
